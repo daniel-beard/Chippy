c0dc86ced02c507c6c8ce0b1a5837f0a9f00092f
dbeard <dbeard@groupon.com>
2022-03-06 17:04:32 -0800

    Add some support for buttons and gates

--- Chippy/Chippy/GameViewController.swift
+++ Chippy/Chippy/GameViewController.swift
@@ -23,7 +23,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadLevel(levelNumber: 3)
+        loadLevel(levelNumber: 4)
     }
 
     override var shouldAutorotate: Bool { false }
---
+++ Chippy/Chippy/GreenButtonTile.swift
@@ -0,0 +1,13 @@
+//
+//  GreenButtonTile.swift
+//  Chippy
+//
+//  Created by Daniel Beard on 3/6/22.
+//  Copyright © 2022 DanielBeard. All rights reserved.
+//
+
+import Foundation
+
+class GreenButtonTile: BaseTile {
+    override func layer() -> TileLayer { .two }
+}
